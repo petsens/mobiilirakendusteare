f3611f110b657838e21a5b41ae0a706afd38706b
Rando Mikksaar <23726279+RandoMikksaar@users.noreply.github.com>
2020-03-30 20:08:15 +0300

    Mood tempo page

--- app/src/main/java/com/example/tujupaevik/TempoPageActivity.java
+++ app/src/main/java/com/example/tujupaevik/TempoPageActivity.java
@@ -1,14 +1,27 @@
 package com.example.tujupaevik;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.marcinmoskala.arcseekbar.ArcSeekBar;
+import com.marcinmoskala.arcseekbar.ProgressListener;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+
+import static com.example.tujupaevik.Functions.addDataToFile;
 
 public class TempoPageActivity extends AppCompatActivity {
+
     private Button button;
+    ArcSeekBar arcSeekBar;
+    private String tempoStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +33,35 @@ public class TempoPageActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openFuelPage();
+                SaveTodaysTempo(tempoStr);
             }
         });
+
+        arcSeekBar = (ArcSeekBar)findViewById(R.id.deafultSeekBar);
+
+        arcSeekBar.setOnProgressChangedListener(new ProgressListener() {
+            @Override
+            public void invoke(int moodTempo) {
+                Log.d("Tempo",""+moodTempo);
+                tempoStr = String.valueOf(moodTempo);
+                button.setText(tempoStr);
+            }
+        });
+    }
+
+    public void SaveTodaysTempo(String data) {
+        try {
+            addDataToFile("Tempo", data, this.getFilesDir());
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void openFuelPage() {
